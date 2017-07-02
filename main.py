eef9cab26f69eef627eb8d15301682263cedc655
JasonOnes <jasonr.jones14@gmail.com>
2017-07-02 11:15:55 -0700

    error msgs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # simple sign-up page with password confirmation and email with feedback
 
 from flask import Flask, redirect, render_template, request
+#import cgi
+
 
 app = Flask(__name__)
 app.config['DEBUG']= True
@@ -9,18 +11,37 @@ app.config['DEBUG']= True
 def sign_up():
     return render_template("index.html")
 
-@app.route("/confirm", methods=['POST'])
+@app.route("/confirm", methods=['GET','POST'])
 def confirm():
-    name = request.form.get('username')
-    psw = request.form.get('passw')
+    name = request.form['username']
+    psw = request.form['passw']
+    #con_psw = request.form['conf_pass']
     con_psw = request.form.get('conf_pass')
-    if psw == con_psw:
-        return render_template("confirm.html", name=name)
+    email = request.form['email']
+    if len(name) not in range(3, 21):
+        error = "Username must be at least 3 characters and no more than 20."
+        #print(error)
+        return render_template("index.html", error=error)
+        #return render_template("index.html", username=error)
+    # elif len(psw) not in range(3, 21):
+    #     error = "Passwords must be at least 3 chars and no more than 20."
+    #     return render_template("index.html", passw=error)
+    # elif " " in name:
+    #     error = "spaces not allowed"
+    #     return render_template("index.html", username=error)
+    # elif " " in psw:
+    #     error = "no spaces allowed in password"
+    #     return render_template("index.html", passw=error)
+    # elif psw != con_psw:
+    #     error = "Passwords must match."
+    #     return render_template("index.html", conf_pass=error)
+    # elif email:
+    #     error = "Not a valid email address!"
+    #     if "@" not in email:
+    #         return render_template("index.html", email=error)
+    #         #more criteria here, use regex?        
     else:
-        # error = "Passwords do not match!"
-        #return redirect("/", username=name, passw=psw, conf_pass="")
-        return render_template("index.html", username=name)#, 'username'=name, passw=psw, conf_pass = '' ) #+ error 
-
+        return render_template("confirm.html", name=name)
 
 
 app.run()
