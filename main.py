f8f227aca650c19c2edffbdd69b36945b33d3fee
JasonOnes <jasonr.jones14@gmail.com>
2017-07-03 11:28:46 -0700

    error msgs done, rerendering with username and email

--- main.py
+++ main.py
@@ -1,47 +1,61 @@
-# simple sign-up page with password confirmation and email with feedback
+""" simple sign-up page with password confirmation and email with feedback """
 
-from flask import Flask, redirect, render_template, request
+from flask import Flask, render_template, request
 #import cgi
 
 
 app = Flask(__name__)
-app.config['DEBUG']= True
+app.config['DEBUG'] = True
 
-@app.route("/", methods=['GET','POST'])
+@app.route("/", methods=['GET', 'POST'])
 def sign_up():
+    """ renders sign up form view"""
     return render_template("index.html")
 
-@app.route("/confirm", methods=['GET','POST'])
+@app.route("/form_inputs", methods=['POST'])
+def print_form_values():
+    """just a check to see form keys"""
+    resp = " "
+    for field in request.form.keys():
+        resp += "<br>{key}</br>: {value}</br>".format(key=field, value=request.form[field])
+    return resp
+
+@app.route("/confirm", methods=['POST'])
 def confirm():
+    """checks to see if inputs valid if so return confirmation view"""
     name = request.form['username']
     psw = request.form['passw']
     #con_psw = request.form['conf_pass']
     con_psw = request.form.get('conf_pass')
     email = request.form['email']
+    #if request.method == 'POST':
     if len(name) not in range(3, 21):
         error = "Username must be at least 3 characters and no more than 20."
-        #print(error)
-        return render_template("index.html", error=error)
-        #return render_template("index.html", username=error)
-    # elif len(psw) not in range(3, 21):
-    #     error = "Passwords must be at least 3 chars and no more than 20."
-    #     return render_template("index.html", passw=error)
-    # elif " " in name:
-    #     error = "spaces not allowed"
-    #     return render_template("index.html", username=error)
-    # elif " " in psw:
-    #     error = "no spaces allowed in password"
-    #     return render_template("index.html", passw=error)
-    # elif psw != con_psw:
-    #     error = "Passwords must match."
-    #     return render_template("index.html", conf_pass=error)
-    # elif email:
-    #     error = "Not a valid email address!"
-    #     if "@" not in email:
-    #         return render_template("index.html", email=error)
-    #         #more criteria here, use regex?        
+        return render_template("index.html", email=email, error1=error)
+    elif " " in name:
+        error = "spaces not allowed"
+        return render_template("index.html", email=email, error1=error)
+    elif len(psw) not in range(3, 21):
+        error = "Passwords must be at least 3 chars and no more than 20."
+        return render_template("index.html", username=name, email=email, error2=error)
+    elif " " in psw:
+        error = "no spaces allowed in password"
+        return render_template("index.html", username=name, email=email, error2=error)
+    elif psw != con_psw:
+        error = "Passwords didn't match!!"
+        return render_template("index.html", username=name, email=email, error3=error)
+    elif email:
+        error = "Not a valid email address!"
+        msg = render_template("index.html", username=name, error4=error)
+        if "@" and "." not in email:
+            return msg
+        elif " " in email:
+            return msg
+        elif email not in range(3, 21):
+            return msg
+        else:
+            return render_template("confirm.html", name=name)
     else:
         return render_template("confirm.html", name=name)
-
 
 app.run()
