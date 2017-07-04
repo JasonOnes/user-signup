2fa2e60c6020ea805063cdd2af00c0e72c24ee55
JasonOnes <jasonr.jones14@gmail.com>
2017-07-03 19:49:47 -0700

    final regex solutions

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 import re
 from flask import Flask, render_template, request
 
-#import cgi
-
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -26,39 +24,27 @@ def confirm():
     """checks to see if inputs valid if so return confirmation view"""
     name = request.form['username']
     psw = request.form['passw']
-    #con_psw = request.form['conf_pass']
-    con_psw = request.form.get('conf_pass')
+    con_psw = request.form['conf_pass']
     email = request.form['email']
-    if len(name) not in range(3, 21):
-        error = "Username must be at least 3 characters and no more than 20."
-        #post_email = request.form.post('email')
+    match = re.compile(r"[^\s-]{3,21}")
+    match_name = match.fullmatch(name)
+    match_psw = match.fullmatch(psw)
+    if not match_name:
+        error = "no spaces allowed and must be between 3 and 20 chars"
         return render_template("index.html", email=email, error1=error)
-    elif " " in name:
-        error = "spaces not allowed"
-        return render_template("index.html", email=email, error1=error)
-    elif len(psw) not in range(3, 21):
-        error = "Passwords must be at least 3 chars and no more than 20."
-        return render_template("index.html", username=name, email=email, error2=error)
-    elif " " in psw:
-        error = "no spaces allowed in password"
-        return render_template("index.html", username=name, email=email, error2=error)
+    elif not match_psw:
+         error = "no spaces allowed and must be between 3 and 20 chars"
+         return render_template("index.html", username=name, email=email, error2=error)
     elif psw != con_psw:
         error = "Passwords didn't match!!"
         return render_template("index.html", username=name, email=email, error3=error)
     elif email:
-        match_pat = re.compile(r"[^@]+@[^@]+\.[^@]+") # tried and tried to get range W/i regex
-        # using ^{3,21}$ but no dice
+        match_pat = re.compile(r"[^@]+@[^@]+\.[^@]+") 
         match = match_pat.match(email)
         error = "Not a valid email address!"
         msg = render_template("index.html", username=name, error4=error)
         if not match or len(email) not in range(3, 21):
             return msg
-        # if "@" and "." not in email:
-        #     return msg
-        # elif " " in email:
-        #     return msg
-        # elif len(email) not in range(3, 21):
-        #     return msg
         else:
             return render_template("confirm.html", name=name)
     else:
